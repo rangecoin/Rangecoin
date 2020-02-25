cd81b29b76461d77c9ee6576398ae8e33ad3df1b
rangecoin <57840163+rangecoin@users.noreply.github.com>
2020-02-25 21:26:20 +0530

    updated

--- src/java/nxt/AssetDividend.java
+++ src/java/nxt/AssetDividend.java
@@ -1,6 +1,7 @@
 /*
  * Copyright © 2013-2016 The Nxt Core Developers.
  * Copyright © 2016-2018 Jelurida IP B.V.
+ * Copyright © 2019-2020 The RNG Core Developers.
  *
  * See the LICENSE.txt file at the top-level directory of this distribution
  * for licensing information.
